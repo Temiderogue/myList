8ff91126d422374eff1522bfe117e2f5cb0f0acb
Temi Mostovoy <mostovoyart@gmail.com>
2019-03-27 21:24:21 +0200

    wow, that is realy huge

---
+++ main.js
@@ -0,0 +1,24 @@
+var addbtn = document.getElementById('addbtn');
+
+addbtn.onclick = function (){
+    var addedstring = prompt("Write your new string here.")
+    if(addedstring){
+        var ol = document.getElementById('myList');
+        var li = document.createElement('li');
+        li.innerHTML = addedstring;
+        ol.appendChild(li);
+        }
+}
+
+delbtn.onclick = function(){
+    var ol = document.getElementById('myList');
+    var lastLi = ol.lastChild;
+    while (lastLi && lastLi.nodeType != 1){
+        lastLi = lastLi.previousSibling;
+        console.log(lastLi)
+     }
+     if (lastLi){
+         lastLi.parentNode.removeChild(lastLi);//удаляем пункт списка, 
+         //если конечно в списке еще что-то осталось
+       }
+}
